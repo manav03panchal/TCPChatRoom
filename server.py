f557ed1706dd0f8583a6cc78b768f50f7775884d
Manav Panchal <manav03panchal@gmail.com>
2023-05-19 16:22:39 -0600

    made client file, wrote recieve() and write()

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import threading
 import socket
 
 host = '127.0.0.1'  # this is the localhost
-port = 55555
+port = 55556
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -38,3 +38,32 @@ def handle(client):
             broadcast(f'{nickname} has left the chat!'.encode('ascii'))
             nicknames.remove(nickname)
             break
+
+# main method to combine all the above functions
+
+
+def receive():
+    while True:
+        # allowing all the clients to connect
+        client, address = server.accept()
+        print(f"Connected with {str(address)}")
+        # only the client can see the below to choose a nickname
+        client.send('NICK'.encode('ascii'))
+        nickname = client.recv(1024).decode('ascii')
+        nicknames.append(nickname)
+        clients.append(client)
+        # print this to the client
+        print(f'Nickname of the client is {nickname}!')
+        # print this to everyone in the current server
+        broadcast(f'{nickname} has joined the chat!'.encode('ascii'))
+        # send this to the client to notify succesful connection
+        client.send("Connected to the server!".encode('ascii'))
+
+        # we will have one thread for each client as we have multiple interactions being handled
+
+        thread = threading.Thread(target=handle, args=(client,))
+        thread.start()
+
+
+print("Server is listening...")
+receive()
